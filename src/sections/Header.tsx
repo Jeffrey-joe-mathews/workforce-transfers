09a52449fb3d51c279abc8baff40d30800ed5077
Jeffrey-joe-mathews <jeffreyjoemathews@gmail.com>
2025-04-26 10:04:36 +0530

    fixed the header for mobile

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -40,7 +40,7 @@ export const Header = () => {
   return (
     <header className="sticky top-0 backdrop-blur-sm z-20">
       <div className="px-5 md:p-5 flex justify-center items-center">
-        <div className="container ">
+        <div className="container py-2">
           <div className="flex justify-between items-center ">
             <Image src="/logo.svg" alt="Company Logo" width={50} height={50} priority />
 
